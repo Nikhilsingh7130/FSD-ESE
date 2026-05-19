f0f7f4c1ff7c9a6e2411f396ab8809eabb92e65e
Nikhil Singh <nikhilsingh01042005@gmail.com>
2026-05-19 12:08:41 +0530

    Switched to HashRouter to fix 404 errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,6 +8,7 @@ import ComplaintRegistration from './components/ComplaintRegistration';
 import ComplaintList from './components/ComplaintList';
 import ComplaintDetails from './components/ComplaintDetails';
 import UpdateStatusPage from './pages/UpdateStatusPage';
+import Layout from './components/Layout';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,9 +16,9 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Layout><App /></Layout>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Layout><Login /></Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/complaints" element={<Layout><ComplaintList /></Layout>} />
         <Route path="/file-complaint" element={<Layout><ComplaintRegistration /></Layout>} />
         <Route path="/complaint/:id" element={<Layout><ComplaintDetails /></Layout>} />
